8bc3f2326df4a83bb4206fef42cbaca9feadc03c
P3T3RALTF4 <148472662+P3T3RALTF4@users.noreply.github.com>
2025-11-07 13:34:01 +0700

    Update A.cpp

--- Codeforces/Round 138 (Div. 2)/A.cpp
+++ Codeforces/Round 138 (Div. 2)/A.cpp
@@ -1,11 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int x,y,z;
+int x,y,z, a[5];
 int main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    cin >> x >> y >> z;
-    int c1 = __gcd(x,y), c2 = __gcd(x,z);
-    cout << c1*4 + c2*4 + (z/c2)*4;
+    cin >> a[1] >> a[2] >> a[3];
+    sort(a+1,a+4);
+    int x = a[1], y = a[2], z = a[3];
+    for (int i = 1;i <= y;i++) {
+        if (y % i == 0 && z % i ==0 ) {
+            int c1 = y / i, c2 = z / i;
+            if (c1*c2 == x) {
+                cout << c1*4 + c2*4 + i*4;
+                return 0;
+            }
+        }
+    }
     return 0;
 }
