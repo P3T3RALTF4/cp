7ead57017319df30252108c13ccc7d7b01058ad8
P3T3RALTF4 <148472662+P3T3RALTF4@users.noreply.github.com>
2025-11-07 13:42:06 +0700

    Update A.cpp

--- Codeforces/Round 125 (Div. 2)/A.cpp
+++ Codeforces/Round 125 (Div. 2)/A.cpp
@@ -6,14 +6,14 @@ ll n,dp[maxn];
 int main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    dp[0] = 1, dp[1] = 1;
-    for (int i = 2;i <= 45;i++) {
+    dp[0] = 0, dp[1] = 1;
+    for (int i = 2;i <= 46;i++) {
         dp[i] = dp[i-1] + dp[i-2];
     }
     cin >> n;
-    for (int i = 1;i <= 45;i++) {
-        for (int j = 1;j <= 45;j++) {
-            for (int k = 1;k <= 45;k++) {
+    for (int i = 0;i <= 46;i++) {
+        for (int j = 0;j <= 46;j++) {
+            for (int k = 0;k <= 46;k++) {
                 if (dp[i] + dp[j] + dp[k] == n) {
                     cout << dp[i] << " " << dp[j] << " " << dp[k];
                     return 0;
